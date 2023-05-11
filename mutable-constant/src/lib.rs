96b93e6f275d32288fe12d570f8e733aef78e5ff
Carlos Kieliszewski <carlos.kieliszewski@gmail.com>
2023-05-11 08:52:53 -0600

    [mutable-constant] Added Mc

---
+++ mutable-constant/src/lib.rs
@@ -0,0 +1,30 @@
+use std::pin::Pin;
+
+
+pub struct Mc<T>(*mut T);
+
+impl<T> Mc<T> {
+    pub fn new(t: T) -> Mc<T> {
+        Mc(Box::into_raw(Box::new(t)))
+    }
+}
+
+impl<T> Drop for Mc<T> {
+    fn drop(&mut self) {
+        unsafe {
+            drop(Box::from_raw(self.0));
+        }
+    }
+}
+
+impl<T> AsRef<T> for Mc<T> {
+    fn as_ref(&self) -> &T {
+        unsafe { &*self.0 }
+    }
+}
+
+impl<T> AsMut<T> for Mc<T> {
+    fn as_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.0 }
+    }
+}
