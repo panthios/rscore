056788089bd1f16758cb7ff3508d773aa1f08573
Carlos Kieliszewski <carlos.kieliszewski@gmail.com>
2023-05-12 05:52:56 -0600

    [moment-node] Added Moment trait

--- moment-node/src/lib.rs
+++ moment-node/src/lib.rs
@@ -1,14 +1,48 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use async_trait::async_trait;
+
+
+#[async_trait]
+pub trait Moment: Clone + Send + Sync {
+    type Output: Moment;
+
+    async fn resolve(&self) -> Self::Output;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+macro_rules! literal_moments {
+    ($($name:ident),*) => {
+        $(
+            #[async_trait]
+            impl Moment for $name {
+                type Output = $name;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+                async fn resolve(&self) -> Self::Output {
+                    *self
+                }
+            }
+        )*
+    }
+}
+
+literal_moments![
+    u8, u16, u32, u64, u128, usize,
+    i8, i16, i32, i64, i128, isize,
+    f32, f64,
+    bool,
+    char
+];
+
+#[async_trait]
+impl<T> Moment for Option<T>
+where
+    T: Moment,
+{
+    type Output = Option<T::Output>;
+
+    async fn resolve(&self) -> Self::Output {
+        if let Some(value) = self {
+            Some(value.resolve().await)
+        } else {
+            None
+        }
     }
 }
