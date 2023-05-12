6e421b55a1ba9740c5fd77a1260a1079deee785c
Carlos Kieliszewski <carlos.kieliszewski@gmail.com>
2023-05-12 08:29:18 -0600

    [moment-node] Added doc comments

--- moment-node/src/lib.rs
+++ moment-node/src/lib.rs
@@ -1,14 +1,77 @@
+#![warn(missing_docs)]
+
+//! # `moment-node`
+//! 
+//! This crate provides a framework for building
+//! trees of asynchronous computations. The actual
+//! calculations are not in this crate. Instead, other
+//! crates can implement the `Moment` trait and use
+//! the `Node` enum to build any kind of tree they
+//! want.
+//! 
+//! ## Example
+//! 
+//! ```
+//! use moment_node::Moment;
+//! use async_trait::async_trait;
+//! use std::ops::Add;
+//! 
+//! #[derive(Clone)]
+//! struct AddOne<T>(pub T)
+//! where
+//!     T: Moment,
+//!     T::Output: Add<i32>;
+//! 
+//! #[async_trait]
+//! impl<T> Moment for AddOne<T>
+//! where
+//!     T: Moment,
+//!     T::Output: Add<i32>,
+//!     <T::Output as Add<i32>>::Output: Moment
+//! {
+//!     type Output = <T::Output as Add<i32>>::Output;
+//! 
+//!     async fn resolve(&self) -> Self::Output {
+//!         self.0.resolve().await + 1
+//!     }
+//! }
+//! 
+//! #[tokio::main]
+//! async fn main() {
+//!     let op = AddOne(10);
+//!     assert_eq!(op.resolve().await, 11);
+//! }
+//! ```
+
 use async_trait::async_trait;
 
-pub mod node;
+mod node;
+pub use node::Node;
 
 mod impls;
 
 
+/// An asynchronously resolvable value.
+/// 
+/// This trait allows any literal or operation
+/// to be used in a `Node` tree. It is not
+/// automatically implemented for any type other
+/// than the ones in the standard library.
 #[async_trait]
 pub trait Moment: Clone + Send + Sync {
+    /// The type of the value that will be resolved.
+    /// 
+    /// If the original type is a generic of some
+    /// kind (e.g. `Vec<T>`), then the output type
+    /// should be the same generic type with resolved
+    /// contents (e.g. `Vec<T::Output>`)
     type Output: Moment;
 
+    /// Resolve the value.
+    /// 
+    /// This method should return the resolved value
+    /// as soon as possible. If the value is already
+    /// resolved, then it should return immediately.
     async fn resolve(&self) -> Self::Output;
 }
 
--- moment-node/src/node.rs
+++ moment-node/src/node.rs
@@ -2,9 +2,32 @@ use crate::Moment;
 use async_trait::async_trait;
 
 
+/// A node in a tree of asynchronous computations.
+/// 
+/// This enum is the core of the `moment-node` crate.
+/// It allows any type that implements the `Moment`
+/// trait to track its progress over time.
+/// 
+/// When resolving a `Node`, the enum state is kept the
+/// same with resolved contents by default. This means the
+/// type changes from `Node<T>` to `Node<T::Output>`. If you
+/// do not want this, use the `Node::into_after` method. This
+/// will keep the same type while resolving, allowing for easier
+/// tree construction in some cases.
 #[derive(Clone)]
 pub enum Node<T: Moment> {
+  /// The node has not been resolved yet.
+  /// 
+  /// This state does not mean the value is not
+  /// finalized. It's just a hint to certain tree
+  /// implementations that the value is not
+  /// guaranteed to be stable.
   Before(T),
+  /// The node has been resolved.
+  /// 
+  /// This state is guaranteed to be stable in a
+  /// best-practices tree implementation. It is
+  /// created with the `Node::into_after` method.
   After(T::Output)
 }
 
@@ -12,10 +35,20 @@ impl<T> Node<T>
 where
   T: Moment
 {
+  /// Create a new node.
+  /// 
+  /// This method is equivalent to `Node::Before(value)`.
   pub fn new(value: T) -> Self {
     Node::Before(value)
   }
 
+  /// Convert the node to the `After` state.
+  /// 
+  /// This should only be done when the value is
+  /// guaranteed to be stable. If the value may change
+  /// on another `resolve`, use the `resolve` method
+  /// instead. This rule is not enforced at compile time,
+  /// so users should be careful when using this method.
   pub async fn into_after(self) -> Self {
     match self {
       Node::Before(value) => Node::After(value.resolve().await),
