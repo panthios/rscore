165cccba4c10b2be2add29ec20868127122e7bf5
Carlos Kieliszewski <carlos.kieliszewski@gmail.com>
2023-05-13 07:28:04 -0600

    [data-relations] Added basic structs

--- data-relations/src/lib.rs
+++ data-relations/src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+
+
+pub struct OneToOne<A, B> {
+    pub from: A,
+    pub to: B
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<A, B> OneToOne<A, B> {
+    pub fn new(from: A, to: B) -> Self {
+        Self { from, to }
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct OneToMany<A, B> {
+    pub from: A,
+    pub to: Vec<B>
+}
+
+impl<A, B> OneToMany<A, B> {
+    pub fn new(from: A, to: Vec<B>) -> Self {
+        Self { from, to }
+    }
+}
+
+pub struct ManyToOne<A, B> {
+    pub from: Vec<A>,
+    pub to: B
+}
+
+impl<A, B> ManyToOne<A, B> {
+    pub fn new(from: Vec<A>, to: B) -> Self {
+        Self { from, to }
+    }
+}
+
+pub struct ManyToMany<A, B> {
+    pub from: Vec<A>,
+    pub to: Vec<B>
+}
+
+impl<A, B> ManyToMany<A, B> {
+    pub fn new(from: Vec<A>, to: Vec<B>) -> Self {
+        Self { from, to }
     }
 }
